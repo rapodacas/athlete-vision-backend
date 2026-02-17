6f5a7130c72c0423b6773ec74328b90862d99f30
rapodacas <raoul.fresno@gmail.com>
2026-02-17 12:34:49 -0800

    Again

--- api/media/delete.js
+++ api/media/delete.js
@@ -1,4 +1,4 @@
-import { withCors } from './_cors.js';
+import { withCors } from '../_cors.js';
 import handler from '../../server/handlers/delete-media.js';
 
 export default withCors(handler);
--- api/teams/list.js
+++ api/teams/list.js
@@ -1,4 +1,4 @@
-import { withCors } from './_cors.js';
+import { withCors } from '../_cors.js';
 import handler from '../../server/handlers/list-teams.js';
 
 export default withCors(handler);
